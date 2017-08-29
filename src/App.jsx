09f7f4939df5221baf5fde84b2d739f93b11a851
David Noriega <dnori817@gmail.com>
2017-08-29 07:08:58 -0400

    Setup routes in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.scss";
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
 	render() {
 		return (
-			<div className="App">
-				<h1>NYCDA Webpack Template</h1>
-				<h2>Replace me!</h2>
-			</div>
+			<BrowserRouter>
+				<div className="App">
+					<Navigation/>
+					<Switch>
+						<Route exact path="/" component={Home}/>
+						<Route exact path="/Quiz" component={Quiz}/>
+						<Route exact path="/Results" component={Results}/>
+						<Route exact path="/Scores" component={Scores}/>
+					</Switch>
+				</div>
+			</BrowserRouter>
 		);
 	}
 }
