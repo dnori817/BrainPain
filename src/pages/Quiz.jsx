f91bcef2917eaca9c619387a3205185339ddcc5b
David Noriega <dnori817@gmail.com>
2017-09-11 22:03:51 -0400

    Show final score on completion

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,6 +1,5 @@
 import "./Quiz.scss";
 import React, { Component } from "react";
-import { bindActionCreators } from 'redux';
 import { Link } from "react-router-dom";
 import { getQuiz } from "actions/quiz";
 import Loader from "components/Loader.jsx";
@@ -8,7 +7,6 @@ import Question from "components/Question.jsx";
 
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-
 
 class Quiz extends Component {
 	constructor(props) {
@@ -33,65 +31,53 @@ class Quiz extends Component {
 
 		console.log(quiz);
 
-
-
-
-
 		let content;
 
 		if (isLoading) {
 			content = <Loader/>;
+		} else if (!quiz) {
+			content = <div className="">{error}</div>;
 		}
-		else if (!quiz) {
-			content = <div className="">{ error }</div>;
-		}
-		else { content = (
-			<div>
+		// else if (currentQuestion >= quiz.length) {
+		// 	content = <div className="result">hi</div>;
+		// }
+		else {
+			content = (
+				<div>
 
-				<div className="All">
+					<div className="All">
 
+						{quiz.map((results) => {
+							return (
+								<div className="container">
+									{currentQuestion >= quiz.length && this._finalScore()}
 
-					{quiz.map((results) => {
-
-						return (
-							<div className="container">
-
-								{ !isLoading && this._renderScore() }
-	                { quiz.length > 0 &&
+									{currentQuestion < quiz.length &&
 										<div>
-											<button className='blue-grey lighten-1 btn next-button' onClick={this._nextQuestion}>Skip Question</button>
-	                    <Question question={quiz[currentQuestion]} 		answerSelected={this._answerSelected.bind(this)}
-
-											 />
+											{this._renderScore()}
+											<button
+												className='blue-grey lighten-1 btn next-button'
+												onClick={this._nextQuestion}
+											>
+											Skip Question
+											</button>
+											<Question
+												question={quiz[currentQuestion]}
+												answerSelected={this._answerSelected.bind(this)}
+											/>
 										</div>
-										}
-								{/* <h5 className="center" dangerouslySetInnerHTML={{
-									__html: results.question,
-								}}
-								/>
-								<div className="center">
-									<button className="waves-effect waves-light btn true">
-										{results.correct_answer}
-									</button>
-									<button className="waves-effect waves-light btn false">
-										{results.incorrect_answers[0]}
-									</button>
-								</div> */}
+									}
 
-
-
-
-							</div>
-						);
-					}
-					)}
+								</div>
+							);
+						})}
+					</div>
 				</div>
-			</div>
-		);
+			);
 		}
 		return (
 			<div className="All">
-				{ content }
+				{content}
 			</div>
 		);
 	}
@@ -100,10 +86,25 @@ class Quiz extends Component {
 		const { currentQuestion, score } = this.state;
 
 		return (
-		    <div className='quiz-score blue-grey lighten-1'>
-		        <p className='count left'>Question {currentQuestion + 1} of {quiz.length}</p>
-						<h4 className="score">Score: { score }</h4>
-		    </div>
+			<div className='quiz-score blue-grey lighten-1'>
+				<p className='count left'>Question {currentQuestion + 1}
+					 of {quiz.length}</p>
+
+					 <h4 className="score">Score: {score}</h4>
+
+			</div>
+		);
+	}
+
+	_finalScore = () => {
+		const { quiz } = this.props;
+		const { currentQuestion, score } = this.state;
+
+		return (
+			<div className='question blue-grey lighten-1'>
+
+				<h1 className="center final-score">FINAL SCORE: {score}</h1>
+			</div>
 		);
 	}
 
@@ -119,13 +120,12 @@ class Quiz extends Component {
 			score = this.state.score + (!isCorrect ? -1 : 0);
 		}
 		// const score = this.state.score + (isCorrect ? 1 : 0);
-setTimeout(() => {
-		this.setState({
-			score,
-			currentQuestion: this.state.currentQuestion + 1,
-
-		});
-}, 250);
+		setTimeout(() => {
+			this.setState({
+				score,
+				currentQuestion: this.state.currentQuestion + 1,
+			});
+		}, 250);
 	};
 
 	_nextQuestion = () => {
@@ -136,17 +136,9 @@ setTimeout(() => {
 	};
 }
 
-
 function mapStateToProps(state, props) {
-	return {
-		quiz: state.quiz.quiz,
-		isLoading: state.quiz.isLoading,
-		error: state.quiz.error,
-	};
+	return { quiz: state.quiz.quiz, isLoading: state.quiz.isLoading, error: state.quiz.error };
 }
 
-// const mapDispatchToProps = (dispatch) => bindActionCreators({
-// 	getQuiz,
-// }, dispatch);
 
-export default connect(mapStateToProps, { getQuiz }) (Quiz);
+export default connect(mapStateToProps, { getQuiz })(Quiz);
